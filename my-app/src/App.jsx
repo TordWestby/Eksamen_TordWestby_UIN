103a52e96c96b57f050c237b35283d94200115e2
TordWestby <tordhw@gmail.com>
2024-04-29 21:13:00 +0200

    !!!
    ymese

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './style/style.css';
+import Layout from './components/layout';
 
 function App() {
   return (
     <>
-      
+      <Layout />
     </>
   )
 }
---
+++ my-app/src/components/navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../style/navbar.css'
+
+export default function Navbar() {
+    return (
+        <>
+        <nav>
+            <ul>
+                <li>
+                    <a href="">test1</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="">test2</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="">test3</a>
+                </li>
+            </ul>
+        </nav>
+        </>
+    )
+}
